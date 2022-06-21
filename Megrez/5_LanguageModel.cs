c4bdba5843e7b06d678989bc939a9530d56a309f
ShikiSuen <shikisuen@outlook.com>
2022-06-21 08:46:16 +0800

    Turn LanguageModel into protocol (interface in CSharp). (#4)

--- Megrez/5_LanguageModel.cs
+++ Megrez/5_LanguageModel.cs
@@ -28,44 +28,29 @@ using System.Linq;
 
 namespace Megrez {
 /// <summary>
-/// 語言模型框架，回頭實際使用時需要派生一個型別、且重寫相關函式。
+/// 語言模型框架協定，實際使用時需要派生一個型別、且重寫相關函式。
 /// </summary>
-public class LanguageModel {
-  /// <summary>
-  /// 語言模型框架，回頭實際使用時需要派生一個型別、且重寫相關函式。
-  /// </summary>
-  public LanguageModel() {}
-
-  // This function works merely as a placeholder.
-  // Please override this function to implement your own methods.
+public interface LanguageModel {
   /// <summary>
   /// 給定鍵，讓語言模型找給一組單元圖陣列。
   /// </summary>
   /// <param name="key">給定鍵。</param>
   /// <returns>一組單元圖陣列。</returns>
-  public virtual List<Unigram> UnigramsFor(string key) {
-    return key.Length == 0 ? new List<Unigram>().ToList() : new();
-  }
+  public List<Unigram> UnigramsFor(string key);
 
-  // This function works merely as a placeholder.
-  // Please override this function to implement your own methods.
   /// <summary>
   /// 給定當前鍵與前述鍵，讓語言模型找給一組雙元圖陣列。
   /// </summary>
   /// <param name="precedingKey">前述鍵。</param>
   /// <param name="key">當前鍵。</param>
   /// <returns>一組雙元圖陣列。</returns>
-  public virtual List<Bigram> BigramsForKeys(string precedingKey, string key) {
-    return precedingKey == key ? new List<Bigram>().ToList() : new();
-  }
+  public List<Bigram> BigramsForKeys(string precedingKey, string key);
 
-  // This function works merely as a placeholder.
-  // Please override this function to implement your own methods.
   /// <summary>
   /// 給定鍵，確認是否有單元圖記錄在庫。
   /// </summary>
   /// <param name="key">給定鍵。</param>
   /// <returns>True 則表示在庫，False 則表示不在庫。</returns>
-  public virtual bool HasUnigramsFor(string key) { return key.Length != 0; }
+  public bool HasUnigramsFor(string key);
 }
 }
