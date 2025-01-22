e5049cdb7eaeae4d8fb0c81de5342b5e841919cc
ShikiSuen <shikisuen@outlook.com>
2025-01-22 15:05:56 +0800

    Patch an issue in JumpCursorBySpan(). (#31)

--- Megrez/src/1_Compositor.cs
+++ Megrez/src/1_Compositor.cs
@@ -283,27 +283,25 @@ public partial struct Compositor {
         if (target == 0) return false;
         break;
     }
-    // var currentRegion = WalkedNodes.CursorRegionMap()[target];  // <- 這樣雖然 C#
-    // 不會建置報錯，但可能會在運行時查詢失敗。 if (WalkedNodes.CursorRegionMap()[target] is not int currentRegion)
-    // return false; <- 這樣是錯的。
-    if (!WalkedNodes.CursorRegionMap().TryGetValue(key: target, out int currentRegion))
-      return false;  // <- 這樣是對的。
+    if (!WalkedNodes.CursorRegionMap().TryGetValue(key: target, out int currentRegion)) return false;
+    int guardedCurrentRegion = Math.Min(WalkedNodes.Count - 1, currentRegion);
     int aRegionForward = Math.Max(currentRegion - 1, 0);
     int currentRegionBorderRear = WalkedNodes.GetRange(0, currentRegion).Select(x => x.SpanLength).Sum();
 
     if (target == currentRegionBorderRear) {
       target = direction switch {
-        TypingDirection.ToFront => currentRegion > WalkedNodes.Count
-                                       ? Keys.Count
-                                       : WalkedNodes.GetRange(0, currentRegion + 1).Select(x => x.SpanLength).Sum(),
+        TypingDirection.ToFront =>
+            currentRegion > WalkedNodes.Count
+                ? Keys.Count
+                : WalkedNodes.GetRange(0, guardedCurrentRegion + 1).Select(x => x.SpanLength).Sum(),
         TypingDirection.ToRear => WalkedNodes.GetRange(0, aRegionForward).Select(x => x.SpanLength).Sum(),
         _ => target
       };
     } else {
-      target =
-          direction switch { TypingDirection.ToFront => currentRegionBorderRear + WalkedNodes[currentRegion].SpanLength,
-                             TypingDirection.ToRear => currentRegionBorderRear,
-                             _ => target };
+      target = direction switch { TypingDirection.ToFront =>
+                                      currentRegionBorderRear + WalkedNodes[guardedCurrentRegion].SpanLength,
+                                  TypingDirection.ToRear => currentRegionBorderRear,
+                                  _ => target };
     }
     switch (isMarker) {
       case false:
